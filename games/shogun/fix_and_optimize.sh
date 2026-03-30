e9a332808f687d167ac2cf8412c3ad8cadcc73d9
ricini343 <rikatoba11@gmail.com>
2026-03-30 20:53:00 +0300

    Add bias field patch to fix script

--- games/shogun/fix_and_optimize.sh
+++ games/shogun/fix_and_optimize.sh
@@ -10,6 +10,25 @@ mkdir -p "$DEST_LIB"
 cp -r "$ENV_LIB"/* "$DEST_LIB"/
 echo "Copied library files OK"
 
+echo "=== Step 1b: Add missing bias field to math_config.json ==="
+MCFG="$DEST_LIB/configs/math_config.json"
+python3 -c "
+import json
+with open('$MCFG') as f:
+    d = json.load(f)
+if 'bias' not in d:
+    d['bias'] = [
+        {'bet_mode': 'base', 'bias': []},
+        {'bet_mode': 'buy_bonus', 'bias': []},
+        {'bet_mode': 'super_buy_bonus', 'bias': []},
+    ]
+    with open('$MCFG', 'w') as f:
+        json.dump(d, f, indent=2)
+    print('Added bias field OK')
+else:
+    print('bias field already exists')
+"
+
 echo "=== Step 2: Source cargo ==="
 . /usr/local/cargo/env
 
